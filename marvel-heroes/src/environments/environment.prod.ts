e5762fb2e48c5498cd381b212a86416725784b3f
alejandron103 <alejandro.naranjo@cun.edu.co>
2022-08-01 17:22:48 -0500

    fix: enviroment prod to deploy

--- marvel-heroes/src/environments/environment.prod.ts
+++ marvel-heroes/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  BASE_API: 'https://gateway.marvel.com:443/v1/public/',
+  API_KEY : '8f750505355890a01761cbe9d299b9ae',
+  PRIVATE_KEY : 'd3cae9978a9d9634cd124daf6db3b3eef6eb0fb3',
+  TIME_STAMP: new Date().getTime(),
 };
